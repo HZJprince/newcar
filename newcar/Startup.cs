fce3f27e4336497c55c6e2b5c090e8c80915bc72
hzj <hzj@CICP-HZJ>
2016-08-15 14:03:47 +0800

    添加项目文件。

---
+++ newcar/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(newcar.Startup))]
+namespace newcar
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
